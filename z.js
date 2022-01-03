265f8f57c972349f5ebe1791bf2842bd78f35504
vExcess <41875927+vExcess@users.noreply.github.com>
2022-01-02 19:39:53 -0800

    Create z.js

---
+++ z.js
@@ -0,0 +1,5 @@
+(function () {
+for (let i = 99; i > 0; i --) {
+    console.log(`${i} bottles of beer on the wall, ${i} bottles of beer, take one down pass it around ${i-1} bottles of beer on the wall`)
+}
+})()
