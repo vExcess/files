6d9af3d53f6dc9c71b2b70c7b060e643eee40256
vExcess <41875927+vExcess@users.noreply.github.com>
2022-01-14 10:00:21 -0800

    Delete z.js

--- z.js
+++
@@ -1,5 +0,0 @@
-(function () {
-for (let i = 99; i > 0; i --) {
-    console.log(`${i} bottles of beer on the wall, ${i} bottles of beer, take one down pass it around ${i-1} bottles of beer on the wall`)
-}
-})()
